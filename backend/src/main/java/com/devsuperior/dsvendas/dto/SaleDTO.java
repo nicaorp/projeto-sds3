d21418cc7c21fadd5589218ac01d9d73f07ad33d
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-04 07:59:11 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -1,42 +1,38 @@
-package com.devsuperior.dsvendas.entities;
+package com.devsuperior.dsvendas.dto;
 
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import com.devsuperior.dsvendas.entities.Sale;
 
-@Entity
-@Table (name = "tb_sales")
-public class Sale {
+public class SaleDTO {
 
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
 	
-	@ManyToOne
-	@JoinColumn(name = "seller_id")
-	private Seller seller;
+	private SellerDTO seller;
 	
-	public Sale() {	
+	public SaleDTO() {
 	}
 
-	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
 		this.id = id;
 		this.visited = visited;
 		this.deals = deals;
 		this.amount = amount;
 		this.date = date;
 		this.seller = seller;
+	}
+	
+	public SaleDTO(Sale entity) {
+		this.id = entity.getId();
+		this.visited = entity.getVisited();
+		this.deals = entity.getDeals();
+		this.amount = entity.getAmmount();
+		this.date = entity.getDate();
+		this.seller = new SellerDTO(entity.getSeller());
 	}
 
 	public Long getId() {
@@ -63,12 +59,12 @@ public class Sale {
 		this.deals = deals;
 	}
 
-	public Double getAmmount() {
+	public Double getAmount() {
 		return amount;
 	}
 
-	public void setAmmount(Double ammount) {
-		this.amount = ammount;
+	public void setAmount(Double amount) {
+		this.amount = amount;
 	}
 
 	public LocalDate getDate() {
@@ -79,16 +75,15 @@ public class Sale {
 		this.date = date;
 	}
 
-	public Seller getSeller() {
+	public SellerDTO getSeller() {
 		return seller;
 	}
 
-	public void setSeller(Seller seller) {
+	public void setSeller(SellerDTO seller) {
 		this.seller = seller;
 	}
 	
 	
 	
-	
-	
 }
+
